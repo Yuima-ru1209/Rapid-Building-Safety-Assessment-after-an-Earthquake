53d7c6f06c58cd1ef71bea73e4e440757d356895
Yuimaru.innovation <yuimaru.innovation1209@gmail.com>
2025-07-16 15:25:48 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const map = L.map('map').setView([34.7303, 136.5086], 10);L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+const map = L.map('map').setView([34.7303, 136.5086], 10);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
